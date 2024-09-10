bb30c9c7cff42e8691149c9a8fcd717da5bf2703
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-09-10 13:54:07 -0300

    Barra de status funcional (para o arquivo 1)

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ const button1 = document.getElementById('button1')
 const button2 = document.getElementById('button2')
 const button3 = document.getElementById('button3')
 
+const statusBar1 = document.getElementById('statusBar1')
+const statusBar2 = document.getElementById('statusBar2')
+const statusBar3 = document.getElementById('statusBar3')
+
+
 const percentage1 = document.getElementById('percentage1')
 const percentage2 = document.getElementById('percentage2')
 const percentage3 = document.getElementById('percentage3')
@@ -18,6 +23,8 @@ button3.addEventListener('click',startDownload)
 function updateStatus() {
     percentageValue+=1
     percentage1.innerHTML = `${percentageValue}%`
+    statusBar1.style.width = `${percentageValue}%`
+
 }
 
 
